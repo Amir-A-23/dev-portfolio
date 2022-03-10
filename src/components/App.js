76bb203c149791493f16119d686737b3a68b0ebd
Amir-A-23 <amir.a.23@hotmail.com>
2022-03-10 17:19:57 -0600

    feat: add spline model

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-
+import { Spline } from '@splinetool/react-spline';
 function App() {
 	return (
-		<div classname='flex w-screen h-screen min-h-screen flex-col item-center justify-center relative bg-primary'></div>
+		<div className='flex w-screen h-screen min-h-screen flex-col item-center justify-center relative bg-primary pb-20'>
+			<div className='relative' id='home'>
+				<Spline scene='https://draft.spline.design/WcKB7mm1w31uZf7n/scene.spline' />
+			</div>
+		</div>
 	);
 }
 
