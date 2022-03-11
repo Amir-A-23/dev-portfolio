5f5487bd3490116d54dcfbd40d6f82a63dc0f066
Amir-A-23 <amir.a.23@hotmail.com>
2022-03-10 23:38:55 -0600

    feat: add about section

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { Spline } from '@splinetool/react-spline';
+import amir from '../assets/images/amir.jpg';
+
 function App() {
 	const [isActive, setIsActive] = useState(false);
 
 	return (
-		<div className='flex w-screen h-screen min-h-screen flex-col item-center justify-center relative bg-primary pb-20'>
+		<div className='flex w-screen min-h-screen flex-col item-center justify-center relative bg-primary pb-20'>
 			{/* Navigation Bar */}
 			<nav className='w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center'>
 				<div className='w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center'>
@@ -92,13 +94,48 @@ function App() {
 				</div>
 			</nav>
 			<div className='relative' id='home'>
-				<Spline scene='https://draft.spline.design/WcKB7mm1w31uZf7n/scene.spline' />
+				<Spline scene='https://prod.spline.design/oo6IxFu8UDjFUmjD/scene.spline' />
 				<div className='absolute bottom-10 w-full justify-center items-center flex'>
 					<div className='shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl'>
-						<p className='text-textBase'>Press and drag to orbit</p>
+						<p className='text-textBase'>
+							Hello, I'm a full-stack developer from Toronto, Canada! 👋🏾
+						</p>
 					</div>
 				</div>
 			</div>
+
+			{/* Main sections */}
+			<main className='w-[80%] mt-4'>
+				{/* About Section */}
+				<section
+					className='w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24'
+					id='about'>
+					{/* image box */}
+					<div className='w-full h-420 flex items-center justify-center'>
+						<div className='w-275 h-340 relative bg-emerald-200 rounded-md'>
+							<img
+								className='w-full h-full absolute -right-4 top-4 object-cover rounded-lg'
+								src={amir}
+								alt=''
+							/>
+						</div>
+					</div>
+					{/* content box */}
+					<div className='w-full h-420 flex flex-col items-center justify-center'>
+						<p className='text-lg text-textBase text-center'>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							enim ad minim veniam, quis nostrud exercitation ullamco laboris
+							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+							reprehenderit in voluptate velit esse cillum dolore eu fugiat
+							nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+							sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+					</div>
+				</section>
+				{/* Skills section */}
+				<section id='skills'>Technical Skills:</section>
+			</main>
 		</div>
 	);
 }
